de56e210414e9202a844820e1311c8370e675f12
Benoit2109 <benoitlebourdonnec@gmail.com>
2020-10-23 21:57:43 +0200

    Updates

---
+++ precache-manifest.86dc9f2bdd1089050b5b53e17bd58ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61da088947e8a1090b8789f5131755c0",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "56b90b92a89a9034279b",
+    "url": "/curriculum/static/css/main.bfaa8cf3.chunk.css"
+  },
+  {
+    "revision": "87e1d4e2a78db85e0712",
+    "url": "/curriculum/static/js/2.e213ad04.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/curriculum/static/js/2.e213ad04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b90b92a89a9034279b",
+    "url": "/curriculum/static/js/main.2340eaaf.chunk.js"
+  },
+  {
+    "revision": "ea8537b673971a69b88d",
+    "url": "/curriculum/static/js/runtime-main.6f6a4d16.js"
+  }
+]);
