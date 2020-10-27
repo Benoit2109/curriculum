3b0e6112968432cef5765c344697162dd1fb047c
Benoit2109 <benoitlebourdonnec@gmail.com>
2020-10-27 12:11:33 +0100

    footer fix

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,8 +4,9 @@ import './Footer.css';
 
 const Footer = () => (
     <div className="footer">
-        <p>Curriculum et Portofolio de <a href="https://github.com/Benoit2109?tab=repositories"> Benoit Le Bourdonnec</a> (oct 2020)</p>
-        <p>benoitlb@me.com - 07 81 29 74 47</p>
+        <p className="footerInfo">Portofolio de <a href="https://github.com/Benoit2109?tab=repositories"> Benoit Le Bourdonnec</a></p>
+        <p>(octobre 2020)</p>
+        <p className="footerInfo">benoitlb@me.com - 07 81 29 74 47</p>
     </div>
 )
 
