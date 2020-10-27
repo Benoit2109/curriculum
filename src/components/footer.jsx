2f6c6d62027416a7d2059541f16c7c2ff87c7240
Benoit2109 <benoitlebourdonnec@gmail.com>
2020-10-27 13:24:33 +0100

    portfolio ok

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 
 const Footer = () => (
     <div className="footer">
-        <p className="footerInfo">Portofolio de <a href="https://github.com/Benoit2109?tab=repositories"> Benoit Le Bourdonnec</a></p>
+        <p className="footerInfo">télécharger le CV de <a href="../image/CVDev10.20.pdf" download> Benoit Le Bourdonnec</a></p>
         <p>(octobre 2020)</p>
         <p className="footerInfo">benoitlb@me.com - 07 81 29 74 47</p>
     </div>
