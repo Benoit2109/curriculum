c8b4a017e703219b166c937e3674082a99b3c4ec
Benoit2109 <benoitlebourdonnec@gmail.com>
2020-10-27 12:01:03 +0100

    add phone and email

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,6 +5,7 @@ import './Footer.css';
 const Footer = () => (
     <div className="footer">
         <p>Curriculum et Portofolio de <a href="https://github.com/Benoit2109?tab=repositories"> Benoit Le Bourdonnec</a> (oct 2020)</p>
+        <p>benoitlb@me.com - 07 81 29 74 47</p>
     </div>
 )
 
